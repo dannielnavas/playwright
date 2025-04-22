2c0dffd4c4423116ab44cb986e9d8ff843e059b5
Danniel Navas <dannielnavas@gmail.com>
2025-04-22 07:56:14 -0500

    feat: :sparkles: agregar pruebas automatizadas para la navegación en el sitio de UITesting

---
+++ tests/uitesting.spec.ts
@@ -0,0 +1,19 @@
+import { test } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("http://uitestingplayground.com/");
+  await page.getByRole("link", { name: "Resources" }).click();
+  await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link", { name: "Dynamic ID" }).click();
+  await page.getByRole("button", { name: "Button with Dynamic ID" }).click();
+  await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link", { name: "Shadow DOM" }).click();
+  await page.locator("#editField").click();
+  await page.getByRole("button", { name: "" }).click();
+  await page.getByRole("button", { name: "" }).click();
+  await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link", { name: "Hidden Layers" }).click();
+  await page.getByRole("button", { name: "Button" }).click();
+  await page.getByText("Some applications use DOM").click();
+  await page.getByRole("link", { name: "Home" }).click();
+});
