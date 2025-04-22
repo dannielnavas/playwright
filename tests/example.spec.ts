f71e57f89466f6fb15ab65e982003a66805c07e0
Danniel Navas <dannielnavas@gmail.com>
2025-04-22 07:39:12 -0500

    feat: :sparkles: reemplazar pruebas de Playwright con un nuevo flujo de navegación en Platzi

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,8 @@
-import { test, expect } from '@playwright/test';
+import { test } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("test", async ({ page }) => {
+  await page.goto("https://platzi.com/");
+  await page.getByRole("button", { name: "Comunidad" }).click();
+  await page.getByRole("link", { name: "Blog" }).click();
+  await page.getByRole("link", { name: "Más contenido! " }).click();
 });
