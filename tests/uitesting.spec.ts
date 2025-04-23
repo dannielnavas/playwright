94922a026f8a1b78b701c631204c95131fff3781
Danniel Navas <dannielnavas@gmail.com>
2025-04-23 07:45:30 -0500

    feat: :sparkles: restaurar y optimizar pruebas en uitesting.spec.ts

--- tests/uitesting.spec.ts
+++ tests/uitesting.spec.ts
@@ -2,24 +2,44 @@ import { expect, test } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("http://uitestingplayground.com/");
-  // // await page.getByRole("link", { name: "Resources" }).click();
-  // await page.locator('a.nav-link:has-text("Resources")').click();
-  // await page.getByRole("link", { name: "Home" }).click();
-  // await page.getByRole("link", { name: "Dynamic ID" }).click();
-  // await page.getByRole("button", { name: "Button with Dynamic ID" }).click();
-  // await page.getByRole("link", { name: "Home" }).click();
-  // await page.getByRole("link", { name: "Shadow DOM" }).click();
-  // await page.locator("#editField").click();
-  // await page.getByRole("button", { name: "" }).click();
-  // await page.getByRole("button", { name: "" }).click();
-  // await page.getByRole("link", { name: "Home" }).click();
-  // await page.getByRole("link", { name: "Hidden Layers" }).click();
-  // await page.getByRole("button", { name: "Button" }).click();
-  // // await page.getByText("Some applications use DOM").click();
-  // await page.locator("text=Some applications use DOM").click();
-  // await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link", { name: "Resources" }).click();
+  await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link", { name: "Dynamic ID" }).click();
+  await page.getByRole("button", { name: "Button with Dynamic ID" }).click();
+  await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link", { name: "Shadow DOM" }).click();
+  await page.locator("#editField").click();
+  await page.getByRole("button", { name: "" }).click();
+  await page.getByRole("button", { name: "" }).click();
+  await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link", { name: "Hidden Layers" }).click();
+  await page.getByRole("button", { name: "Button" }).click();
+  await page.getByText("Some applications use DOM").click();
+  await page.getByRole("link", { name: "Home" }).click();
+});
 
-  // new test
+test("test locator", async ({ page }) => {
+  await page.goto("http://uitestingplayground.com/");
+  // await page.getByRole("link", { name: "Resources" }).click();
+  await page.locator('a.nav-link:has-text("Resources")').click();
+  await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link", { name: "Dynamic ID" }).click();
+  await page.getByRole("button", { name: "Button with Dynamic ID" }).click();
+  await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link", { name: "Shadow DOM" }).click();
+  await page.locator("#editField").click();
+  await page.getByRole("button", { name: "" }).click();
+  await page.getByRole("button", { name: "" }).click();
+  await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link", { name: "Hidden Layers" }).click();
+  await page.getByRole("button", { name: "Button" }).click();
+  // await page.getByText("Some applications use DOM").click();
+  await page.locator("text=Some applications use DOM").click();
+  await page.getByRole("link", { name: "Home" }).click();
+});
+
+test("test selectors", async ({ page }) => {
+  await page.goto("http://uitestingplayground.com/");
   await page.locator("#title");
   await page.locator(".alert");
   await page.locator("a.navbar-brand").click();
