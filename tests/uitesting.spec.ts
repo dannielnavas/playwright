63e84ba25af4e27fa532f4dfcd9a32d7bbb066d3
Danniel Navas <dannielnavas@gmail.com>
2025-04-23 07:23:00 -0500

    feat: :sparkles: actualizar pruebas de navegación en UITesting para usar selectores más específicos

--- tests/uitesting.spec.ts
+++ tests/uitesting.spec.ts
@@ -2,7 +2,8 @@ import { test } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("http://uitestingplayground.com/");
-  await page.getByRole("link", { name: "Resources" }).click();
+  // await page.getByRole("link", { name: "Resources" }).click();
+  await page.locator('a.nav-link:has-text("Resources")').click();
   await page.getByRole("link", { name: "Home" }).click();
   await page.getByRole("link", { name: "Dynamic ID" }).click();
   await page.getByRole("button", { name: "Button with Dynamic ID" }).click();
@@ -14,6 +15,7 @@ test("test", async ({ page }) => {
   await page.getByRole("link", { name: "Home" }).click();
   await page.getByRole("link", { name: "Hidden Layers" }).click();
   await page.getByRole("button", { name: "Button" }).click();
-  await page.getByText("Some applications use DOM").click();
+  // await page.getByText("Some applications use DOM").click();
+  await page.locator("text=Some applications use DOM").click();
   await page.getByRole("link", { name: "Home" }).click();
 });
