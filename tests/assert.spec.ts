a70652c195b24b6654b333bd3f0f53887944c157
Danniel Navas <dannielnavas@gmail.com>
2025-04-23 07:45:40 -0500

    feat: :sparkles: agregar pruebas de aserción en assert.spec.ts

---
+++ tests/assert.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "@playwright/test";
+
+test("test assert", async ({ page }) => {
+  await page.goto("http://uitestingplayground.com/textinput");
+
+  // verify input is  visible
+  await expect(page.locator("#newButtonName")).toBeVisible();
+  // select input a fill the input you text
+  await page.locator("#newButtonName").fill("Hello World");
+  // click in the button
+  await page.locator("#updatingButton").click();
+  // verify button text updated
+  await expect(page.locator("#updatingButton")).toContainText("Hello World");
+});
